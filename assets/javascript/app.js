1be07ab47906e2848eee62734934204f3defd46e
Christian Escobar <crs.escobar@gmail.com>
2017-12-17 10:06:06 -0800

    added firebase configuration

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCo2DejadWQS7fw_niSEzmhYmnRFMjUSAA",
+	authDomain: "rpsmultiplayer-70853.firebaseapp.com",
+	databaseURL: "https://rpsmultiplayer-70853.firebaseio.com",
+	projectId: "rpsmultiplayer-70853",
+	storageBucket: "rpsmultiplayer-70853.appspot.com",
+	messagingSenderId: "728920962116"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
