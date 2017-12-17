890909ce8d24d35726dc3cb66ded00f249179792
Christian Escobar <crs.escobar@gmail.com>
2017-12-17 15:47:09 -0800

    added logic to handle players joining game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,11 +16,22 @@ var rpsImages = ['./assets/images/rock.png','./assets/images/paper.png','./asset
 
 var rpsImagesPos = 1;
 
+var currentPlayer = 0;
+
 // Listeners
-$('#start-btn').on('click', function() {
+$('#play-btn').on('click', function() {
 	var userName = $('#user-name-input').val().trim();
 
-	addPlayerToDatabase(userName);
+	newPlayer(userName);
+});
+
+database.ref('/players').on('child_added', function(snapshot) {
+	var newPlayer = snapshot.ref.key;
+
+	var newPlayerName = snapshot.val().name;
+
+	setupPlayerPanel(newPlayerName, newPlayer);
+
 });
 
 $('#scroll-left').on('click', function() {
@@ -49,7 +60,7 @@ $('#scroll-right').on('click', function() {
 
 });
 
-function addPlayerToDatabase(userName) {
+function newPlayer(userName) {
 	database.ref('/players').once('value', function(snapshot) {
 		var newPlayer = {
 			name: userName,
@@ -57,23 +68,38 @@ function addPlayerToDatabase(userName) {
 			losses: 0,
 		};
 
-		console.log(snapshot.hasChild('1'));
-
 		if(!snapshot.hasChildren()) {
 			// Add user as player 1
 			database.ref('/players').child('1').set(newPlayer);
 
-			$('#player-value').text(userName + ' you are player 1');
+			currentPlayer = 1;
 		}
 		else if(snapshot.hasChild('1') && !snapshot.hasChild('2')) {
 			// Add user as player 2
 			database.ref('/players').child('2').set(newPlayer);
 
-			$('#player-value').text(userName + ' you are player 2!');
+			currentPlayer = 2;
 		}
 		else {
 			// Two players already exist in database
 			$('#player-value').text('Enough players have joined the game.');
 		}
+
+		if(currentPlayer > 0) {
+			// Setup player panel
+			setupPlayerPanel(userName, currentPlayer);
+
+			// Hider user name input
+			$('#user-name-container').css('display', 'none');
+		}
 	});	
+}
+
+function setupPlayerPanel(userName, player) {
+	var panelTitleId = '#player' + player + '-panel-title';
+	var panelBodyId = '#player' + player + '-panel-body';
+
+	$(panelTitleId).text(userName + ' - Player ' + player);
+
+	$(panelBodyId).empty();
 }
