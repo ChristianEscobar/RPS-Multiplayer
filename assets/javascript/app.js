bb5c0c99fbad11bac10d1d6bf9916c9f3214203a
Christian Escobar <crs.escobar@gmail.com>
2017-12-19 12:08:50 -0800

    game logic work inprogress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,8 +18,6 @@ var rpsImages = ['./assets/images/rock.png','./assets/images/paper.png','./asset
 var rpsImagesPos = 1;
 
 var currentPlayer = '0';
-
-var currentTurn = 0;
 // End Globals //
 
 var selectionsDisplayed = false;
@@ -47,16 +45,16 @@ database.ref('/players').on('child_added', function(snapshot) {
 // Used to handle turn value 
 database.ref().on('child_added', function(snapshot){
 	if(snapshot.key === 'turn') {
-		checkWhoseTurnItIs(snapshot.val());
+		checkWhoseTurnItIs();
 	}
 });
 
 // Listens to the database for children changed.
-// Used to handle selection of rock/paper/scissors
-database.ref('/players').on('child_changed', function(snapshot) {
-	updateTurnValue();
-
-	checkWhoseTurnItIs(currentTurn);
+// Used to handle turn value changes
+database.ref().on('child_changed', function(snapshot) {
+	if(snapshot.ref.key === 'turn') {
+		checkWhoseTurnItIs();	
+	}
 });
 
 $(document).on('click', '.scroll-btn', scrollImages);
@@ -97,20 +95,16 @@ function selectionMade() {
 	// Remove buttons from panel body
 	updatePlayerPanelBody(currentPlayer, false, true, $('#selection-img').attr('src'), '');
 
+	// Player has made a selection, so the turn value has to be updated.
 	updateTurnValue();
 
-	checkWhoseTurnItIs(currentTurn);
-}
-
-function setTurnMessageWaitingForPlayer(player) {
-	database.ref('/players/' + player).once('value').then(function(snapshot) {
-			setTurnMessage('Waiting for ' + snapshot.val().name + ' to choose...');
-		});
+	//checkWhoseTurnItIs();
 }
 
 // Adds a new player to the database
 function addNewPlayer(userName) {
-	database.ref('/players').once('value', function(snapshot) {
+	database.ref('/players').once('value')
+		.then(function(snapshot) {
 		var newPlayer = {
 			name: userName,
 			wins: 0,
@@ -156,61 +150,161 @@ function addNewPlayer(userName) {
 }
 
 function updateTurnValue() {
-	currentTurn++;
+	database.ref().once('value')
+		.then(function(snapshot) {
 
-	var turnObj = {
-		turn: currentTurn,
-	}
+		var newTurnValue = 0;
 
-	updateDatabaseNode('', turnObj);
+		if(snapshot.child('turn').exists()) {
+			// Read turn value, update, then write
+			var currentTurnValue = snapshot.child('turn').val();
+
+			newTurnValue = currentTurnValue + 1;
+		} else {
+			// Add turn value
+			newTurnValue = 1;
+		}
+
+		var turnObj = {
+			turn: newTurnValue,
+		};
+
+		updateDatabaseNode('', turnObj);
+
+	});
 }
 
-function checkWhoseTurnItIs(currentTurn) {
-	console.log('currentTurn', currentTurn);
-
+function checkWhoseTurnItIs() {
 	// There are only 3 turns per game:
 	// 1. Player 1 is choosing and Player 2 is waiting
 	// 2. Player 1 has chosen and Player 2 is choosing
 	// 3. Both players have chosen, determine winner
-	if(currentTurn === 1) {
-		// Player 1 is choosing while Player 2 is waiting
-		if(currentPlayer === '1') {
-			setTurnMessage('It\'s Your Turn!');
+	database.ref().once('value')
+		.then(function(snapshot){
+			var player1Obj = snapshot.child('/players/1').val();
+			var player2Obj = snapshot.child('/players/2').val();
 
-			// Display scrollable images
-			updatePlayerPanelBody(currentPlayer, true, false, '', '');
+			if(snapshot.child('turn').val() === 1) {
+				// Player 1 is choosing while Player 2 is waiting
+				if(currentPlayer === '1') {
+					setTurnMessage('It\'s Your Turn!');
 
-			// Update Player 2 panel to waiting
-			updatePlayerPanelBody('2', false, false, '', 'Waiting for your selection...');
+					// Display scrollable images
+					updatePlayerPanelBody(currentPlayer, true, false, '', '');
 
+					// Update Player 2 panel to waiting
+					updatePlayerPanelBody('2', false, false, '', 'Waiting for your selection...');
+
+				} else {
+					// You are Player 2, so you have to wait for Player 1
+					setTurnMessage('Waiting for ' + player1Obj.name + ' to choose...');
+
+
+					// Update Player 1 panel to selecting
+					updatePlayerPanelBody('1', false, false, '', 'Selecting...');
+				}
+			} else if(snapshot.child('turn').val() === 2) {
+				// Player 1 has chosen and is now waiting, Player 2 is choosing
+				if(currentPlayer === '1') {
+					// Waiting for Player 2
+					setTurnMessage('Waiting for ' + player2Obj.name + ' to choose...');
+
+					// Update Player 2 panel to selecting
+					updatePlayerPanelBody('2', false, false, '', 'Selecting...');
+				} else {
+					setTurnMessage('It\'s Your Turn!');
+
+					// Display scrollable images
+					updatePlayerPanelBody(currentPlayer, true, false, '', '');
+
+					// Update Player 1 panel to waiting
+					updatePlayerPanelBody('1', false, false, '', 'Waiting for your selection...');
+				}
+			} else if(snapshot.child('turn').val() === 3) {
+				// Determine winner
+				determineWinner();
+			}
+		});
+}
+
+function determineWinner() {
+	database.ref('/players').once('value')
+		.then(function(snapshot) {
+
+		var player1Obj = snapshot.child('1').val();
+		var player2Obj = snapshot.child('2').val();
+
+		var winner = '';
+
+		if(player1Obj.choice === player2Obj.choice) {
+			// It's a tie!!!!
+			winner = 'Tie';
+		} else if(hasPlayer1Won(player1Obj.choice, player2Obj.choice)) {
+			winner = '1';
+		} else if(hasPlayer2Won(player1Obj.choice, player2Obj.choice)) {
+			winner = '2';
 		} else {
-			// You are Player 2, so you have to wait
-			setTurnMessageWaitingForPlayer('1');
+			console.log('determineWinner():  Something went wrong!  Unable to determine winner based on player choice.');
 
-			// Update Player 1 panel to selecting
-			updatePlayerPanelBody('1', false, false, '', 'Selecting...');
+			return;
 		}
-	} else if(currentTurn === 2) {
-		// Player 1 has chosen and is now waiting, Player 2 is choosing
+
+		// Display opponents choice
 		if(currentPlayer === '1') {
-			// Waiting for Player 2
-			setTurnMessageWaitingForPlayer('2');
+			// Display Player 2's choice
+			var imageUrl = getChoiceImageUrlBasedOnValue(player2Obj.choice);
 
-			// Update Player 2 panel to selecting
-			updatePlayerPanelBody('2', false, false, '', 'Selecting...');
-		} else {
-			setTurnMessage('It\'s Your Turn!');
+			updatePlayerPanelBody('2', false, true, imageUrl, '');
+		} else if(currentPlayer === '2') {
+			// Display Player 1's choice
+			var imageUrl = getChoiceImageUrlBasedOnValue(player1Obj.choice);
 
-			// Display scrollable images
-			updatePlayerPanelBody(currentPlayer, true, false, '', '');
-
-			// Update Player 1 panel to waiting
-			updatePlayerPanelBody('1', false, false, '', 'Waiting for your selection...');
+			updatePlayerPanelBody('1', false, true, imageUrl, '');
 		}
-	} else if(currentTurn === '3') {
-		// Determine winner
-		
+
+		// Update turn message
+		if(winner === currentPlayer) {
+			setTurnMessage('Awesome, you won!');
+
+			displayYouWinResult(); 
+		} else {
+			if(currentPlayer === '1') {
+				setTurnMessage(player2Obj.name + ' has beat you.  Better luck next time.');	
+
+				displayYouWLoseResult();
+			} else if(currentPlayer === '2') {
+				setTurnMessage(player1Obj.name + ' has beat you.  Better luck next time.');
+
+				displayYouWLoseResult();
+			} else {
+				console.log('checkWhoseTurnItIs():  Unhandled currentPlayer value encoutered ' + currentPlayer);
+			}		
+		}
+	});
+}
+
+function hasPlayer1Won(player1Choice, player2Choice) {
+	if(player1Choice === 'rock' && player2Choice === 'scissors') {
+		return true;
+	} else if(player1Choice === 'paper' && player2Choice === 'rock') {
+		return true;
+	} else if(player1Choice === 'scissors' && player2Choice === 'paper') {
+		return true;
 	}
+
+	return false;
+}
+
+function hasPlayer2Won(player1Choice, player2Choice) {
+	if(player2Choice === 'rock' && player1Choice === 'scissors') {
+		return true;
+	} else if(player2Choice === 'paper' && player1Choice === 'rock') {
+		return true;
+	} else if(player2Choice === 'scissors' && player1Choice === 'paper') {
+		return true;
+	}
+
+	return false;
 }
 
 // Updates a specific path in the database with the specified data object
@@ -267,6 +361,19 @@ function getChoiceValue() {
 	}
 }
 
+function getChoiceImageUrlBasedOnValue(choice) {
+	switch(choice) {
+		case 'rock' :
+			return rpsImages[0];
+		case 'paper' :
+			return rpsImages[1];
+		case 'scissors' :
+			return rpsImages[2];
+		default:
+			console.log('getChoiceImageUrlBasedOnValue():  Unhandled choice value encoutered ' + choice);
+	}
+}
+
 function updatePlayerPanelBody(player, displayScrollableImages, displayStaticImage, imageUrl, message) {
 	var panelBodyId = '#player' + player + '-panel-body';
 
@@ -312,4 +419,26 @@ function updatePlayerPanelBody(player, displayScrollableImages, displayStaticIma
 	} else {
 		$(panelBodyId).text(message);
 	}	
+}
+
+function displayYouWinResult() {
+	$('#results-panel-body').empty();
+
+	var img = $('<img/>');
+	img.attr('src', './assets/images/you-win.png');
+	img.attr('alt', 'you win image');
+	img.addClass('img-responsive');
+
+	$('#results-panel-body').append(img);
+}
+
+function displayYouWLoseResult() {
+	$('#results-panel-body').empty();
+
+	var img = $('<img/>');
+	img.attr('src', './assets/images/you-lose.jpg');
+	img.attr('alt', 'you lose image');
+	img.addClass('img-responsive');
+
+	$('#results-panel-body').append(img);
 }
